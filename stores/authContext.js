2fca9027c0e03475bff481392ab04fc964114df0
richardingwe <richardingwe5@gmail.com>
2021-09-28 03:51:26 +0100

    added a logout button and function

--- stores/authContext.js
+++ stores/authContext.js
@@ -17,12 +17,17 @@ export const AuthContextProvider = ({ children }) => {
             netlifyIdentity.close();
             console.log('login event');
         });
+        netlifyIdentity.on('logout', () => {
+            setUser(null);
+            console.log('logout event');
+        });
 
         // init netlify identity connection
         netlifyIdentity.init();
 
         return () => {
             netlifyIdentity.off('login');
+            netlifyIdentity.off('logout');
         };
     }, []);
 
@@ -30,7 +35,11 @@ export const AuthContextProvider = ({ children }) => {
         netlifyIdentity.open();
     };
 
-    const context = { user, login };
+    const logout = () => {
+        netlifyIdentity.logout();
+    };
+
+    const context = { user, login, logout };
 
     return (
         <AuthContext.Provider value={context}>
