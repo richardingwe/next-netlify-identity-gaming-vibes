9d94a928c167325a347ae160c93e12e05fac2dc7
richardingwe <richardingwe5@gmail.com>
2021-09-28 03:18:25 +0100

    added the login function

--- stores/authContext.js
+++ stores/authContext.js
@@ -1,22 +1,39 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
 
 const AuthContext = createContext({
     user: null,
     login: () => { },
     logout: () => { },
-    authReady: fasle
+    authReady: false
 });
 
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        //init netlify identity connection
+        netlifyIdentity.on('login', (user) => {
+            setUser(user);
+            netlifyIdentity.close();
+            console.log('login event');
+        });
+
+        // init netlify identity connection
         netlifyIdentity.init();
+
+        return () => {
+            netlifyIdentity.off('login');
+        };
     }, []);
+
+    const login = () => {
+        netlifyIdentity.open();
+    };
+
+    const context = { user, login };
+
     return (
-        <AuthContext.Provider value={user}>
+        <AuthContext.Provider value={context}>
             {children}
         </AuthContext.Provider>
     );
