1391db1dab56f52e1ee44d3c7e5f8ee9712e2d95
richardingwe <richardingwe5@gmail.com>
2021-09-28 03:25:49 +0100

    added the login/signup button to the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import AuthContext from '../stores/authContext';
 import { useContext } from 'react';
+import AuthContext from '../stores/authContext';
 
 export default function Navbar() {
-  const user = useContextntext(AuthContext);
+  const { user, login } = useContext(AuthContext);
+  console.log(user);
 
   return (
     <div className="container">
@@ -14,6 +15,7 @@ export default function Navbar() {
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li onClick={login} className="btn">Login/Signup</li>
         </ul>
       </nav>
       <div className="banner">
